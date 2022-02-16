e6bb3adf16a32d1b7ae8459edf719ffb83df0f1a
Marvin Bornstein <mbornstein@palantir.com>
2022-02-16 18:02:38 +0000

    initial game version

---
+++ src/config.js
@@ -0,0 +1,24 @@
+export const points = {
+  "E": 1, "N": 1, "I": 1, "R": 1, "S": 1,
+  "T": 1, "A": 1, "D": 2, "H": 2, "U": 2,
+  "L": 2, "C": 3, "G": 3, "M": 3, "O": 1,
+  "B": 3, "W": 3, "F": 3, "K": 3, "Z": 4,
+  "V": 3, "P": 3, "J": 5, "X": 6, "Y": 6,
+  "Q": 7,
+}
+
+export const die = [
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+  "ABCDEF",
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {points, die} from './config.js';
+
+
+const cols = 4;
+const grid = 8;
+const diceWidth = 48;
+
+function startingPosition(index) {
+  return {
+    x: (index % cols) * (diceWidth + grid),
+    y: parseInt(index / cols) * (diceWidth + grid)
+  }
+}
+
+function Dice(props) {
+  return (
+    <div className="dice">
+      {props.value}
+      <sub>{points[props.value]}</sub>
+    </div>
+  )
+}
+
+function Menu(props) {
+  return (
+    <div className="menu">
+      <div className="button" onClick={props.onStart}>
+        <img className='icon' src='random.svg'></img>
+      </div>
+    </div>
+  )
+}
+
+function Field(props) {
+  return (
+    <div className="field">
+    </div>
+  )
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roll: die.map(() => parseInt(Math.random() * 6)),
+      positions: die.map((dice, index) => startingPosition(index)),
+    }
+  }
+
+  handleRestart() {
+    this.setState({
+      roll: die.map(() => parseInt(Math.random() * 6)),
+      positions: die.map((dice, index) => startingPosition(index)),
+    });
+  }
+
+  handleStopDragging(diceIndex, event, coreData) {
+    let x = coreData.lastX;
+    let y = coreData.lastY;
+    let positions = this.state.positions.slice();
+    positions[diceIndex] = {x, y};
+    this.setState({positions});
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <Menu onStart={() => this.handleRestart()}/>
+        {/* <Field /> */}
+        {die.map((d, i) => (
+          <Draggable
+            onStop={(event, coreData) => this.handleStopDragging(i, event, coreData)}
+            key={i}
+            grid={[grid, grid]}
+            position={this.state.positions[i]}
+          >
+            <div><Dice value={d[this.state.roll[i]]}/></div>
+          </Draggable>
+        ))}
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
