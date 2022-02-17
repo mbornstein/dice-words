d21a228b1f56eb9d5d9e1e3273ac2fff0067a297
Marvin Bornstein <mbornstein@palantir.com>
2022-02-17 10:57:47 +0000

    add timer and real dice

--- src/index.js
+++ src/index.js
@@ -17,20 +17,63 @@ function startingPosition(index) {
 }
 
 function Dice(props) {
+  const point = points[props.value];
   return (
     <div className="dice">
       {props.value}
-      <sub>{points[props.value]}</sub>
+      <sub>{point > 0 ? point : ''}</sub>
     </div>
   )
+}
+
+class Timer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: '00:00',
+    }
+  }
+    
+  componentDidMount() {
+    this.intervalID = setInterval(() =>
+      this.updateClock(),
+      1000
+    );
+  }
+    
+  componentWillUnmount(){
+    clearInterval(this.intervalID);
+  }
+    
+    //This function set the state of the time to a new time
+  updateClock(){
+    this.setState({
+      time: this.displayTime(Date.now() - this.props.startTime)
+    });
+  }
+
+  displayTime(ms) {
+    let s = parseInt(ms / 1000);
+    const pad = (t) => t < 10 ? `0${t}` : `${t}`;
+    let mins = parseInt(s / 60);
+    let secs = s % 60;
+    return `${pad(mins)}:${pad(secs)}`;
+  }
+  
+  render() {
+    return (
+      <div className="time">{this.state.time}</div>
+    );
+  }
 }
 
 function Menu(props) {
   return (
     <div className="menu">
       <div className="button" onClick={props.onStart}>
-        <img className='icon' src='random.svg'></img>
+        <img className='icon' src={`${process.env.PUBLIC_URL}/random.svg`}></img>
       </div>
+      <Timer startTime={props.startTime}/>
     </div>
   )
 }
@@ -48,6 +91,7 @@ class Game extends React.Component {
     this.state = {
       roll: die.map(() => parseInt(Math.random() * 6)),
       positions: die.map((dice, index) => startingPosition(index)),
+      startTime: Date.now(),
     }
   }
 
@@ -55,6 +99,7 @@ class Game extends React.Component {
     this.setState({
       roll: die.map(() => parseInt(Math.random() * 6)),
       positions: die.map((dice, index) => startingPosition(index)),
+      startTime: Date.now(),
     });
   }
 
@@ -69,18 +114,20 @@ class Game extends React.Component {
   render() {
     return (
       <div className="game">
-        <Menu onStart={() => this.handleRestart()}/>
-        {/* <Field /> */}
         {die.map((d, i) => (
           <Draggable
-            onStop={(event, coreData) => this.handleStopDragging(i, event, coreData)}
-            key={i}
-            grid={[grid, grid]}
-            position={this.state.positions[i]}
+          onStop={(event, coreData) => this.handleStopDragging(i, event, coreData)}
+          key={i}
+          grid={[grid, grid]}
+          position={this.state.positions[i]}
           >
             <div><Dice value={d[this.state.roll[i]]}/></div>
           </Draggable>
         ))}
+        <Menu
+          onStart={() => this.handleRestart()}
+          startTime={this.state.startTime}
+        />
       </div>
     );
   }
